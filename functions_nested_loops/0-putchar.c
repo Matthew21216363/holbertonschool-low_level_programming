0d0cebc4e5a48a87b1f5fddd629459286c75bd89
Matthew21216363 <matthewmontgomery2163@aol.com>
2023-05-30 15:37:58 -0700

    yuk

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main- print "_putchar:
+ *
+ * Return: Always 0 (Succes)
+ */
+int main(void)
+{
+	_putchar('_')
+	_putchar('p')
+	_putchar('u')
+	_putchar('t')
+	_putchar('c')
+	_putchar('h')
+	_putchar('a')
+	_putchar('r')
+	Return (0);
+}
