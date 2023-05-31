046241715220b5dd0629a0257337dd4f7296be4c
Matthew21216363 <matthewmontgomery2163@aol.com>
2023-05-30 17:45:08 -0700

    yuk

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * print_alphabet_x10 - entry point
+ */
+void print_alphabet_x10(void)
+{
+	char c[] = "abcdefghijklmnopqrstuvwxyz";
+	int m;
+	int y = 1;
+
+	while (y <= 10)
+	{
+		for (m = 0; x < 26; m++)
+		{
+			_putchar(c[m]);
+		}
+	_putchar('\n');
+	y++;
+	}
+}
