855af29ff6668ff4b710169f88800768c6c4f045
Matthew21216363 <matthewmontgomery2163@aol.com>
2023-05-30 18:38:55 -0700

    yuk

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * add - CHECK YOUR PROTOTYPE
+ * @a: CHECK YOUR VAR
+ * @b: CHECK YOUR VAR
+ * Return: CHECK YOUR PROTOTYPE
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
