c92f2017ceac9d86de3af13b4a4bb3ae4edd8112
Matthew21216363 <matthewmontgomery2163@aol.com>
2023-06-06 12:40:52 -0700

    tuk

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,17 +1,19 @@
-#ifndef MAIN H
-#define MAIN H
-#include ‹stdio.h>
-#include ‹unistd.h>
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
 int _putchar(char c);
 void reset_to_98(int *n);
-void swap int(int *a, int *b);
-int strlen(char *s);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
-void rev string (char *s);
+void rev_string(char *s);
 void puts2(char *str);
-void puts half (char *str);
-void print _array(int *a, int n);
-char * strcpy(char *dest, chan
-*src);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+
 #endif
