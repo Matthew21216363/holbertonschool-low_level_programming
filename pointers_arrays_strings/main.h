d02390b69e5b5eb09d7fdc6b13507219418a397c
Matthew21216363 <matthewmontgomery2163@aol.com>
2023-06-05 21:25:45 -0700

    tuk

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -2,17 +2,16 @@
 #define MAIN H
 #include ‹stdio.h>
 #include ‹unistd.h>
-int _putchar (char c) ;
-void reset to 98 (int *n) ;
-void swap int(int
-*a, int *b);
-int strlen(char *s) ;
-void _puts (char *str);
-void print_ rev(char *s);
+int _putchar(char c);
+void reset_to_98(int *n);
+void swap int(int *a, int *b);
+int strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
 void rev string (char *s);
 void puts2(char *str);
 void puts half (char *str);
 void print _array(int *a, int n);
 char * strcpy(char *dest, chan
-*src) ;
+*src);
 #endif
