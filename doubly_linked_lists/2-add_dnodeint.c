10e82b73a6bd0af7eff69106f259d4ea450fb232
root <matthewmontgomery2163@aol.com git config --global user.name Matthew21216363clearcdcdgit add .git commit -m wikgit config --global user.email matthewmontgomery2163@aol.com>
2023-07-24 17:42:16 -0700

    aik

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - adds a node at the beginning of a list
+ * @head: double pointer to head of list
+ * @n: data in list
+ * Return: address or NULL
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	/* Create pointer to new node */
+	dlistint_t *new_node;
+
+	/* Allocate memory for new node */
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	/* Add new node to list */
+	new_node->n = n;
+	new_node->next = *head;
+	new_node->prev = NULL;
+
+	/* If the list isn't empty, update prv to new node */
+	if (*head != NULL)
+	{
+		(*head)->prev = new_node;
+	}
+
+	/* Make new node the head */
+	*head = new_node;
+
+	return (new_node);
+}
